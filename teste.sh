49fbde01ccffa4478697dcf885008c487aef2e55
Luan-MB <luanmb20@gmail.com>
2022-04-27 23:11:30 -0300

    falta o relatorio ;-;

---
+++ teste.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for threads in 1 2 4
+do
+    echo ${threads}
+    for size in 10000 20000 40000 80000 160000
+    do
+
+        python3 input_generator.py ${threads} > input.in
+
+        echo $size
+        for i in {1..20..1}
+        do
+            echo $i
+            mpirun --oversubscribe -np ${threads} ./parallel ${size}.in >> dados/test_${size}_${threads}procs.txt
+        done
+    done
+done
